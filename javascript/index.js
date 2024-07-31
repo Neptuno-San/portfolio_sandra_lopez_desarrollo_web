d09d4e4c74e90219b5820efe8b808605fcee5bb3
Neptuno-San <sandra.lopez.nep@gmail.com>
2024-07-31 13:43:10 -0300

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+
+/* menu lateral */
+
+function desplegarMenu() {
+          document.getElementById('menu-desplegado').className = 'menu-desplegado-activo';
+         document.getElementById('menu-colapsado').className = 'menu-colapsado-oculto'; 
+}
+
+function colapsarMenu() {
+          document.getElementById('menu-desplegado').className = 'menu-desplegado-oculto';
+         document.getElementById('menu-colapsado').className = 'menu-colapsado-activo'; 
+}
+
+/* fin menu lateral */
