5c04506283989be8c3c15312963ed1f0ea8da28f
Neptuno-San <sandra.lopez.code@gmail.com>
2024-08-07 12:00:30 -0300

    correo

--- enviar.php
+++ enviar.php
@@ -9,14 +9,14 @@ $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por: " . $nombre . " \r\n";
-$mensaje = "Su email es: " . $email . " \r\n";
-$mensaje = "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje = "Enviado el: " . date('d/m/Y', time());
+$mensaje .= "Su email es: " . $email . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'sandra.lopez.game@gmail.com';
 $asunto = 'Asunto del mensaje';
 
-email($para, $asunto, utf8_decode($mensaje), $header);
+mail($para, $asunto, utf8_decode($mensaje), $header);
 
 header("Location:index.html");
 
